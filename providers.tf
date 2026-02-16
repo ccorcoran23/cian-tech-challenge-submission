38cff01fa3ca881e3c0f692576a2c176f6e9d6ba
Cian Corcoran <ccorcoran@sentient.ie>
2026-02-16 12:16:37 +0000

    Initial Terraform AWS provider, region var and account sanity output

---
+++ providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+# Provider we use pinned to vendor and version
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Uses default region set in variables.tf
+provider "aws" {
+  region = var.aws_region
+}
